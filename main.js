bfc9aae1726f9188e51d586a2753e4040dd45562
petraalpigiano <petraalpigiano@gmail.com>
2025-04-10 18:31:01 +0200

    add select

--- main.js
+++ main.js
@@ -2,12 +2,12 @@
 const nameInput = document.getElementById("name-input");
 const kmInput = document.getElementById("km-input");
 const ageInput = document.getElementById("age-input");
-const formInput = document.getElementById("form-input");
-// const buttonInput = document.getElementById("button-input");
+// const formInput = document.getElementById("form-input");
+const buttonInput = document.getElementById("button-input");
+const buttonReset = document.getElementById("button-reset");
 
 // Form trigger
-formInput.addEventListener("submit", function (event) {
-  event.defaultPrevented();
+buttonInput.addEventListener("click", function () {
   // Get value from input
   const nameSurname = nameInput.value;
   const age = ageInput.value;
@@ -28,9 +28,9 @@ formInput.addEventListener("submit", function (event) {
   // console.log("ticket for underage", underageDiscount);
   // console.log("ticket for senior", seniorDiscount);
 
-  if (age < 18) {
+  if (age === "Minorenne") {
     console.log("con sconto del 20%:", underageDiscount.toFixed(2));
-  } else if (age > 65) {
+  } else if (age === "Over 65") {
     console.log("con sconto del 40%:", seniorDiscount.toFixed(2));
   } else {
     console.log("ticket prize:", ticketPrice.toFixed(2));
